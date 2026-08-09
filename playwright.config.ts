69a54eeeca944b3b6d965c57761e03d01c173e57
Ashwin1105 <59497970+Ashwin1105@users.noreply.github.com>
2026-08-09 18:31:15 +0530

    QA Automation: playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
+  use: {
+    baseURL: 'http://host.docker.internal:5176',
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ]
+});
