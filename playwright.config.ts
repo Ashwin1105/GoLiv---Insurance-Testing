7845e06fc762268add089ec053352cbc167f5d2c
Ashwin1105 <59497970+Ashwin1105@users.noreply.github.com>
2026-09-14 21:58:46 +0530

    QA Automation: playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,23 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
   timeout: 30 * 1000,
+  testDir: './tests',
   expect: {
     timeout: 5000,
   },
   fullyParallel: true,
   reporter: [
     ['list'],
-    ['allure-playwright'],
+    ['html'],
+    // Uncomment the line below if Allure reporting is set up in the project
+    // ['allure-playwright'],
   ],
   use: {
     baseURL: 'http://localhost:5176',
     headless: false,
     screenshot: 'only-on-failure',
-    trace: 'retain-on-failure',
+    trace: 'on-first-retry',
     video: 'retain-on-failure',
   },
   projects: [
@@ -23,6 +25,18 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
       },
     },
   ],
